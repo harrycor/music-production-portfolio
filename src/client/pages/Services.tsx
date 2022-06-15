d0cbe73340b5720e928bbade55f175d5fc43d459
Harry Corradi <harrycorradi@protonmail.com>
2022-06-15 12:16:55 -0400

    added more data to services

--- src/client/pages/Services.tsx
+++ src/client/pages/Services.tsx
@@ -7,11 +7,13 @@ const Services = () => {
   let returnCardsForServices = (title: string, info: string) => {
     return (
       <div
-        className="col-4 card services-cards-caontainer"
+        className="col-md-3 col-9 p-3 card services-cards-caontainer"
         style={{ backgroundColor: "black" }}
       >
         <div className="cardbody services-cards-info">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title mb-3" style={{ fontSize: "25px" }}>
+            {title}
+          </h5>
           <p className="card-text card-subtitle" style={{ color: "gray" }}>
             {info}
           </p>
@@ -30,27 +32,31 @@ const Services = () => {
     infosix: string
   ) => {
     return (
-      <div className="card col-6" style={{ backgroundColor: "black" }}>
+      <div className="card col-12" style={{ backgroundColor: "black" }}>
         <div className="card-body d-flex flex-wrap">
-          <h5 className="card-title col-12">{title}</h5>
-          <p className="card-text col-6" style={{ color: "gray" }}>
-            {infoOne}
-          </p>
-          <p className="card-text col-6" style={{ color: "gray" }}>
-            {infotwo}
-          </p>
-          <p className="card-text col-6" style={{ color: "gray" }}>
-            {infothree}
-          </p>
-          <p className="card-text col-6" style={{ color: "gray" }}>
-            {infofour}
-          </p>
-          <p className="card-text col-6" style={{ color: "gray" }}>
-            {infofive}
-          </p>
-          <p className="card-text col-6" style={{ color: "gray" }}>
-            {infosix}
-          </p>
+          <h5 className="card-title col-12" style={{ fontSize: "25px" }}>
+            {title}
+          </h5>
+          <div className="col-12 d-flex justify-content-center flex-wrap">
+            <p className="card-text col-md-6 col-12" style={{ color: "gray" }}>
+              {infoOne}
+            </p>
+            <p className="card-text col-md-6 col-12" style={{ color: "gray" }}>
+              {infotwo}
+            </p>
+            <p className="card-text col-md-6 col-12" style={{ color: "gray" }}>
+              {infothree}
+            </p>
+            <p className="card-text col-md-6 col-12" style={{ color: "gray" }}>
+              {infofour}
+            </p>
+            <p className="card-text col-md-6 col-12" style={{ color: "gray" }}>
+              {infofive}
+            </p>
+            <p className="card-text col-md-6 col-12" style={{ color: "gray" }}>
+              {infosix}
+            </p>
+          </div>
         </div>
       </div>
     );
@@ -63,71 +69,87 @@ const Services = () => {
         alt=""
         height={"100%"}
         width={"100%"}
-        style={{ position: "fixed", zIndex: "-1" }}
+        style={{ position: "sticky", top:"0", zIndex: "-1" }}
       />
-      <div style={{ color: "white", backgroundColor: "black" }}>
+      <div style={{ color: "white", paddingBottom: "35%"}}>
         <div
           className="services-container card text-center"
           style={{ backgroundColor: "black" }}
         >
           <div>
-            <h3>Services</h3>
+            <h3 style={{ fontSize: "60px", paddingBottom:"10px" }}>Services</h3>
           </div>
           <div className="d-flex flex-wrap">
-            {returnCardsForServices(
-              "Music Production",
-              "from indipendant artists to bands, from singles to albums. Lets bring your music to life!"
-            )}
-            {returnCardsForServices(
-              "Mixing & Mastering",
-              "make your music stand out with quality sounding audio for any platform or device"
-            )}
-            {returnCardsForServices(
-              "Music Composition",
-              "expand and develop your music or create something entirely new"
-            )}
-            {returnCardsForServices(
-              "Music Scoring",
-              "take the viewing experience to the next level by adding soundscapes and textures"
-            )}
+            <div className="d-flex flex-wrap justify-content-around col-12">
+              {returnCardsForServices(
+                "Music Production",
+                "from indipendant artists to bands, from singles to albums. Lets bring your music to life!"
+              )}
+              {returnCardsForServices(
+                "Mixing & Mastering",
+                "make your music stand out with quality sounding audio for any platform or device"
+              )}
+              {returnCardsForServices(
+                "Music Composition",
+                "expand and develop your music or create something entirely new"
+              )}
+            </div>
+            <div className="d-flex flex-wrap justify-content-around col-12">
+              {returnCardsForServices(
+                "Music Scoring",
+                "take the viewing experience to the next level by adding soundscapes and textures"
+              )}
 
-            {returnCardsForServices(
-              "Sound Design",
-              "add depth depth to your project with SFX, Foley, Ambiance, and more"
-            )}
-            {returnCardsForServices(
-              "Audio Editing",
-              "improve and incorporate your own audio in your own way"
-            )}
+              {returnCardsForServices(
+                "Sound Design",
+                "add depth depth to your project with SFX, Foley, Ambiance, and more"
+              )}
+              {returnCardsForServices(
+                "Audio Editing",
+                "improve and incorporate your own audio in your own way"
+              )}
+            </div>
           </div>
         </div>
 
         <div
           className="studio-container card text-center"
-          style={{ backgroundColor: "black" }}
+          style={{ backgroundColor: "black", marginTop:"45%" }}
         >
           <div>
-            <h3>Studio</h3>
+            <h3 style={{ fontSize: "60px" }}>Studio</h3>
           </div>
           <div className="d-flex flex-wrap">
-            {returnsCardForStudio(
-              "DAWS & Tools",
-              "Protools",
-              "iZotope",
-              "Universal Audio",
-              "Soundtoys",
-              "Native Instruments",
-              "EWQL"
-            )}
-            {returnsCardForStudio(
-              "Intruments",
-              "Guitar",
-              "Piano",
-              "Bass",
-              "Synthesizer",
-              "Ukele",
-              "percussion"
-            )}
+            <div className="d-flex flex-wrap justify-content-around col-md-6 col-12">
+              {returnsCardForStudio(
+                "DAWS & Tools",
+                "Protools",
+                "iZotope",
+                "Universal Audio",
+                "Soundtoys",
+                "Native Instruments",
+                "EWQL"
+              )}
+            </div>
+            <div className="d-flex flex-wrap justify-content-around col-md-6 col-12">
+              {returnsCardForStudio(
+                "Intruments",
+                "Guitar",
+                "Piano",
+                "Bass",
+                "Synthesizer",
+                "Ukele",
+                "percussion"
+              )}
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="d-flex flex-wrap justify-content-around col-12">
+            <div className="d-flex flex-wrap justify-content-center card-bodycol-5" style={{backgroundColor: "black", marginTop:"20%"}}>
+              <h5 className="col-12 text-center">Can't find the service for you?</h5>
+              <button className="btn btn-primary">Contact me!</button>
+            </div>
           </div>
         </div>
       </div>
